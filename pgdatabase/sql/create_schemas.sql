23f744d4126c175a46acdf8b58302b23fad2d29a
Maxwel de Souza Freitas <maxwel@maxwelfreitas.com.br>
2025-11-04 21:10:48 -0300

    refactor(sql): ♻️ Improve permission granting in create_schemas.sql

--- pgdatabase/sql/create_schemas.sql
+++ pgdatabase/sql/create_schemas.sql
@@ -22,20 +22,20 @@ BEGIN
             -- Adiciona comentário ao esquema
             EXECUTE format('COMMENT ON SCHEMA %I IS %L', schema_name, schema_comment);
             
-            -- Concede permissões
+            -- Concede permissões ao esquema
             EXECUTE format('GRANT USAGE ON SCHEMA %I TO PUBLIC', schema_name);
             EXECUTE format('GRANT ALL ON SCHEMA %I TO cdr_database_users', schema_name);
+
+            -- Concede permissões para objetos no esquema
+            EXECUTE format('GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA %I TO cdr_database_users', schema_name);
+            EXECUTE format('GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA %I TO cdr_database_users', schema_name);
+            EXECUTE format('GRANT ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA %I TO cdr_database_users', schema_name);
+            EXECUTE format('GRANT ALL PRIVILEGES ON ALL TYPES IN SCHEMA %I TO cdr_database_users', schema_name);
             
-            -- Altera privilégios padrão para tabelas
+            -- Altera permissões padrão para objetos futuros no esquema
             EXECUTE format('ALTER DEFAULT PRIVILEGES FOR ROLE admin IN SCHEMA %I GRANT ALL ON TABLES TO cdr_database_users', schema_name);
-            
-            -- Altera privilégios padrão para sequências
             EXECUTE format('ALTER DEFAULT PRIVILEGES FOR ROLE admin IN SCHEMA %I GRANT ALL ON SEQUENCES TO cdr_database_users', schema_name);
-            
-            -- Altera privilégios padrão para funções
             EXECUTE format('ALTER DEFAULT PRIVILEGES FOR ROLE admin IN SCHEMA %I GRANT EXECUTE ON FUNCTIONS TO cdr_database_users', schema_name);
-            
-            -- Altera privilégios padrão para tipos
             EXECUTE format('ALTER DEFAULT PRIVILEGES FOR ROLE admin IN SCHEMA %I GRANT USAGE ON TYPES TO cdr_database_users', schema_name);
             
             RAISE NOTICE 'Permissões configuradas para o esquema %', schema_name;
